a9bd619e9d931b7a67a25edf3b3d04264f85f32f
Matthieudegny <matthieu.dev.degny@gmail.com>
2022-08-07 20:15:31 +0200

    dynamics request working on

--- src/components/dynamicRequest/DynamicRequest.js
+++ src/components/dynamicRequest/DynamicRequest.js
@@ -1,24 +1,43 @@
 import React, { useRef, useState } from 'react'
+import { useCatDynamic } from '../../requests/requests'
 
 export const DynamicRequest = () => {
 
-    const [cats, setCats ] = useState('')
+    const catNumberRef = useRef()
 
-    const catNumberRef = useRef('')
-   
-    const submit = () => {
-        setCats(catNumberRef.current)
+    const [numberCats, setNumberCats] = useState()
+
+    const submit = (event) => {
+        event.preventDefault();
+        const numberOfCats = catNumberRef.current.value
+        setNumberCats(numberOfCats)
+        console.log(numberCats)
+        cats(numberCats)
     }
+
+    const { mutate: cats, data } = useCatDynamic(numberCats)
+    //const { mutate : cats , data } = useCatDynamic(numbercats)
+
+  
+    if (data) console.log(data)
+
+    //const arrayCats = data.data
 
     return (
     <div className='container-dynamicRequest'>
 
-        <form action="" onSubmit={submit}>
+        <form action="" onSubmit={(e) => submit(e)}>
 
-            <label for="tentacles">Number of Cats (1-5):</label>
-            <input type="number" name="catNumber" min="1" max="5" placeholder='1' ref={catNumberRef}/>
+            <label htmlFor="tentacles">Number of Cats (1-5):</label>
+            <input type="number" name="catNumber" min="1" max="5" placeholder='0' ref={catNumberRef}/>
 
-            <button onClick={submit}>Ready to adopt</button>
+            <button type='submit'>Ready to adopt</button>
+
+            {data?.data.map((cat) => {
+                return (
+                    <img key={cat.id} src={cat.url} alt="" />
+                )
+            })}
 
         </form>
     </div>
--- src/requests/requests.js
+++ src/requests/requests.js
@@ -1,8 +1,14 @@
-import { useQuery } from 'react-query'
+import { useQuery, useMutation } from 'react-query'
 import axios from 'axios'
 
 const fetchCat = async() => {
     return await axios.get('https://api.thecatapi.com/v1/images/search')
+}
+
+const fetchSeveralCats = async(numberCats) => {
+    console.log(`https://api.thecatapi.com/v1/images/search?limit=${numberCats}`)
+    console.log(numberCats, typeof(numberCats))
+    return await axios.get(`https://api.thecatapi.com/v1/images/search?limit=${numberCats}`)
 }
 
 //request on click
@@ -21,5 +27,15 @@ export const useCatDataMount = (onSuccess, onError) => {
     refetchOnWindowFocus: false,
     onSuccess,
     onError,
+    })
+}
+
+//request on the mount of the component
+export const useCatDynamic = (numberCats) => {
+    return useMutation(['several-cats',numberCats],() => fetchSeveralCats(numberCats), {
+        enabled: false,
+        keepPreviousData: false,
+        refetchOnMount: true,
+        refetchOnWindowFocus: false,
     })
 }
